2e75377320c9ee6869d1d59b2b8a8ad175bbd35e
sonia08 <soniabansal@Sonias-MacBook-Pro.local>
2018-04-03 14:37:24 +1000

    checking with heroku

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/login' =>'session#new'  #sign in frm
   post '/login' => 'session#create' #sign in action
   delete '/login' =>'session#destroy' #sign out
-
+  
   resources :foodtypes #plural
   resources :recipes #plural
 
