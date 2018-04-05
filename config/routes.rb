65fac419b8543161c5e6dc50991470a2954526ce
sonia08 <soniabansal@Sonias-MacBook-Pro.local>
2018-04-05 21:43:36 +1000

    about and contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+
   get '/recipes/types_index' => 'recipes#types_index'
   get '/recipes/types_index/:type' => 'recipes#by_type'
   get '/recipes/favoriterecipe' => 'recipes#favoriterecipe'
   get '/recipes/famousrecipe' => 'recipes#famousrecipe'
+  get '/myprofile' => 'pages#myprofile'
+  get '/contactus' => 'pages#contactus'
   root :to =>'pages#home'
   resources :users, :only => [:new, :create]
   get '/login' =>'session#new'  #sign in frm
